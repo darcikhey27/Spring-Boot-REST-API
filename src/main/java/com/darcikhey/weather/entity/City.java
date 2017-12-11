19d091ab6f4a98e1f4247a996d88c7be96199677
Darci K Saucedo <darcikhey27@gmail.com>
2017-12-11 01:49:23 -0800

    added entity and Beans

--- src/main/java/com/darcikhey/weather/entity/City.java
+++ src/main/java/com/darcikhey/weather/entity/City.java
@@ -1,4 +1,23 @@
 package com.darcikhey.weather.entity;
 
 public class City {
+
+    private String name;
+    private int cityId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
 }
--- src/main/java/com/darcikhey/weather/repository/CityRepository.java
+++ src/main/java/com/darcikhey/weather/repository/CityRepository.java
@@ -1,4 +1,10 @@
 package com.darcikhey.weather.repository;
 
+import java.util.HashMap;
+
 public class CityRepository {
+
+    private static HashMap<Integer, String> citiesRepository;
+
+
 }
