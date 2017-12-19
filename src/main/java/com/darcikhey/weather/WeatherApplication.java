b2a723ff8aff2164f420d5cfd92920bcfd952faa
Darci Khey Saucedo <darcikhey27@gmail.com>
2017-12-18 19:05:00 -0800

    after creating war file

--- src/main/java/com/darcikhey/weather/WeatherApplication.java
+++ src/main/java/com/darcikhey/weather/WeatherApplication.java
@@ -2,11 +2,18 @@ package com.darcikhey.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WeatherApplication {
+public class WeatherApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WeatherApplication.class);
 	}
 }
